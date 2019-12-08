3e3daee2564613121f2e2a6debb7d63eb114ea19
JanMa <git@janma.tk>
2019-12-08 16:06:41 +0100

    Day four - part two

--- day-4/main.go
+++ day-4/main.go
@@ -11,13 +11,26 @@ const (
 )
 
 func main() {
+	fmt.Printf("Part one: ")
 	partOne()
+	fmt.Printf("Part two: ")
+	partTwo()
 }
 
 func partOne() {
 	count := 0
 	for i := Min; i <= Max; i++ {
 		if Match(i) {
+			count++
+		}
+	}
+	fmt.Println(count)
+}
+
+func partTwo() {
+	count := 0
+	for i := Min; i <= Max; i++ {
+		if Two(i) {
 			count++
 		}
 	}
@@ -48,4 +61,27 @@ func Match(num int) bool {
 	}
 
 	return true
+}
+
+func Two(num int) bool {
+	d1 := num % 10
+	d2 := num % 100 / 10
+	d3 := num % 1000 / 100
+	d4 := num % 10000 / 1000
+	d5 := num % 100000 / 10000
+	d6 := num % 1000000 / 100000
+
+	twoMatching := false
+	if d1 == d2 && d2 != d3 ||
+		d1 != d2 && d2 == d3 && d3 != d4 ||
+		d2 != d3 && d3 == d4 && d4 != d5 ||
+		d3 != d4 && d4 == d5 && d5 != d6 ||
+		d4 != d5 && d5 == d6 {
+		twoMatching = true
+	}
+	inc := false
+	if d6 <= d5 && d5 <= d4 && d4 <= d3 && d3 <= d2 && d2 <= d1 {
+		inc = true
+	}
+	return twoMatching && inc
 }
